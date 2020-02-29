38768daba3bc64fdcd07e91c15c35370d30d634d
hongjun7 <hongjun-7@daum.net>
2020-02-29 12:43:01 +0900

    update MNIST solver.

--- Kaggle/digit_recognizer/solver.py
+++ Kaggle/digit_recognizer/solver.py
@@ -9,11 +9,11 @@ from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 simplefilter("ignore")
 
-train_data = pd.read_csv('C:\\Users\\hongj\\Desktop\\kaggle\\digit_recognizer\\train.csv')
+train_data = pd.read_csv('C:\\Users\\hongj\\Desktop\\Kaggle\\digit_recognizer\\train.csv')
 train_labels = train_data['label']
 train_labels = to_categorical(train_labels, num_classes=10)
 train_data = np.array(train_data.drop('label', axis=1))
-test_data = pd.read_csv('C:\\Users\\hongj\\Desktop\\kaggle\\digit_recognizer\\test.csv')
+test_data = pd.read_csv('C:\\Users\\hongj\\Desktop\\Kaggle\\digit_recognizer\\test.csv')
 test_data = np.array(test_data)
 
 train_images = train_data.reshape((42000, 28, 28, 1))
@@ -25,57 +25,53 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 #print(train_images.shape, val_images.shape, train_labels.shape, val_labels.shape)
 
 data_generator = ImageDataGenerator(
-    featurewise_center=False,
-    samplewise_center=False,
-    featurewise_std_normalization=False,
-    samplewise_std_normalization=False,
-    zca_whitening=False,
     rotation_range=10,
     zoom_range=0.1,
     width_shift_range=0.1,
-    height_shift_range=0.1,
-    horizontal_flip=False,
-    vertical_flip=False)
+    height_shift_range=0.1)
 data_generator.fit(train_images)
 
-model = models.Sequential([
-    layers.Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=(28, 28, 1)),
-    layers.Conv2D(32, (5, 5), padding='same', activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Dropout(0.25),
-    layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
-    layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Dropout(0.25),
-    layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
-    layers.Flatten(),
-    layers.Dense(256, activation='relu'),
-    layers.Dense(10, activation='softmax')
-])
+n = 7
+model, history = [0] * n, [0] * n
+for i in range(n):
+    model[i] = models.Sequential([
+        layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(28, 28, 1)),
+        layers.BatchNormalization(),
+        layers.Conv2D(32, (3, 3), padding='same', activation='relu'),
+        layers.BatchNormalization(),
+        layers.Conv2D(32, (3, 3), padding='same', activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.4),
+        layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        layers.BatchNormalization(),
+        layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        layers.BatchNormalization(),
+        layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.3),
+        layers.Flatten(),
 
-model.summary()
-model.compile(optimizer="adam",
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+        layers.Dense(128, activation='relu'),
+        layers.BatchNormalization(),
+        layers.Flatten(),
+        layers.Dropout(0.2),
+        layers.Dense(10, activation='softmax')
+    ])
+    model[i].compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy'])
+    history[i] = model[i].fit_generator(data_generator.flow(train_images, train_labels, batch_size=86), epochs=45,
+                                        # validation_data=(val_images, val_labels),
+                                        verbose=2,
+                                        steps_per_epoch=train_images.shape[0]//86)
 
-history = model.fit_generator(data_generator.flow(train_images, train_labels, batch_size=86), epochs=30,
-                              # validation_data=(val_images, val_labels),
-                              verbose=2,
-                              steps_per_epoch=train_images.shape[0]//86)
-'''
-fig, ax = plt.subplots(2, 1)
-ax[0].plot(history.history['loss'], color='b', label="Training loss")
-ax[0].plot(history.history['val_loss'], color='r', label="validation loss", axes=ax[0])
-legend = ax[0].legend(loc='best', shadow=True)
+count = np.zeros((test_images.shape[0], 10))
+for i in range(n):
+    current = np.argmax(np.array(model[i].predict(test_images, verbose=2)), axis=1)
+    for j in range(test_images.shape[0]):
+        count[j][current[j]] += 1
 
-ax[1].plot(history.history['acc'], color='b', label="Training accuracy")
-ax[1].plot(history.history['val_acc'], color='r', label="Validation accuracy")
-legend = ax[1].legend(loc='best', shadow=True)
-plt.show()
-'''
-predict = np.array(model.predict(test_images, verbose=2))
-predict = np.argmax(predict, axis=1)
+predict = list(np.argmax(count, axis=1))
+
 submission = pd.DataFrame()
-submission['ImageId'] = [i for i in range(1, 28001)]
+submission['ImageId'] = [i for i in range(1, test_images.shape[0]+1)]
 submission['Label'] = predict
-submission.to_csv("C:\\Users\\hongj\\Desktop\\kaggle\\digit_recognizer\\submission.csv", index=False)
+submission.to_csv("C:\\Users\\hongj\\Desktop\\Kaggle\\digit_recognizer\\submission.csv", index=False)
